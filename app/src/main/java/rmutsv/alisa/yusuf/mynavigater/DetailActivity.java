9e033fa9fc5900c81a51a3b64d1a07b86015ac3a
MasterUNG <phrombutr@gmail.com>
2017-07-29 22:43:58 +0700

    Get Location OK

--- app/src/main/java/rmutsv/alisa/yusuf/mynavigater/DetailActivity.java
+++ app/src/main/java/rmutsv/alisa/yusuf/mynavigater/DetailActivity.java
@@ -1,8 +1,16 @@
 package rmutsv.alisa.yusuf.mynavigater;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,11 +34,17 @@ public class DetailActivity extends FragmentActivity implements OnMapReadyCallba
     private TextView nameTextView, dateTextView, distanceTextView;
     private String[] latStrings, lngStrings;
     private String tag = "24JulyV1";
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private double latADouble, lngADouble;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        //Setup Constant
+        setupConstant();
 
         //Get Value From Intent
         getValueFromIntent();
@@ -53,8 +67,115 @@ public class DetailActivity extends FragmentActivity implements OnMapReadyCallba
         //Edit Controller
         editController();
 
+        //Loop Controller
+        loopController();
+
 
     }   // Main Method
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkLatLng();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
+    }
+
+    public Location myFindLocation(String strProvider) {
+
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(strProvider)) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return null;
+            }
+            locationManager.requestLocationUpdates(strProvider, 1000, 10, locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
+        }
+
+        return location;
+    }
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            latADouble = location.getLatitude();
+            lngADouble = location.getLongitude();
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    private void setupConstant() {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        latADouble = 7.087465;
+        lngADouble = 100.245960;
+    }
+
+    private void loopController() {
+
+        //Todo
+        checkLatLng();
+
+
+        //Delay
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                loopController();
+            }
+        }, 3000);
+
+    }   // loopController
+
+    private void checkLatLng() {
+
+        Log.d("29JulyV1", "checkLatLng Work");
+        Location netWorkLocation = myFindLocation(LocationManager.NETWORK_PROVIDER);
+        if (netWorkLocation != null) {
+            latADouble = netWorkLocation.getLatitude();
+            lngADouble = netWorkLocation.getLongitude();
+        }
+
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER);
+        if (gpsLocation != null) {
+            latADouble = gpsLocation.getLatitude();
+            lngADouble = gpsLocation.getLongitude();
+        }
+
+        Log.d("29JulyV1", "lat ==> " + latADouble);
+        Log.d("29JulyV1", "lng ==> " + lngADouble);
+
+    }
 
     private void editController() {
         ImageView imageView = (ImageView) findViewById(R.id.imvEdit);
